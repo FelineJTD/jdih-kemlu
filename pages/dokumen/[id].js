a7d6d175a6a39b58dadc36b929674e0c9c31ee94
FelineJTD <16520335@std.stei.itb.ac.id>
2022-10-04 08:31:12 +0700

    add [id].js

---
+++ pages/dokumen/[id].js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Template from '../../components/layout-template'
+
+export default function DetailDokumen() {
+  const paths = [
+    {
+      name: 'Dokumen Hukum',
+      link: '/dokumen'
+    }
+  ]
+  return (
+    <div>
+      <Head>
+        <title>JDIH Kemlu</title>
+        <meta name="description" content="JDIH Kemlu" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Template
+          title='Dokumen Hukum'
+          subtitle='Himpunan Dokumen Hukum di JDIH Kementrian Luar Negeri'
+          img='https://picsum.photos/200/300'
+          alt='Gambar Dokumen Hukum'
+          paths={paths}
+        >
+          <h2>TEST</h2>
+        </Template>
+      </main>
+    </div>
+  )
+}

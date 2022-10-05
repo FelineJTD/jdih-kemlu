b59ca84cc1dfefdefad58f8fb5368f0834749af2
FelineJTD <16520335@std.stei.itb.ac.id>
2022-10-05 15:19:40 +0700

    complete main hero

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
   const string_to_url = (string) => {
     return string.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
   }
+  
+  const HasilPencarian = ['Permenlu', 'UUD 1945', 'PKS', 'UU / Perppu', 'Perpres']
 
   const DokumenTerbaru = ['PERMENLU NO 67 Tahun 2021', 'PERMENLU NO 6 Tahun 2021', 'Perjanjian Kerja Sama antara Kemenlu dengan Universitas Indonesia Nomor 13/XII/2021']
 
@@ -86,20 +88,50 @@ export default function Home() {
       </Head>
 
       <main>
+        {/* KRITIK & SARAN FLOATING COMPONENT */}
         <ComponentKrisar />
+
         {/* MAIN HERO SECTION */}
-        <section className='bg-[url("/images/homepage/img-background-hero.png")] lg:h-[calc(100vh-60px)] bg-cover bg-center bg-no-repeat relative text-white text-center'>
+        <section className='bg-[url("/images/homepage/img-background-hero.png")] lg:h-screen bg-cover bg-center bg-no-repeat relative text-white text-center -mt-[60px]'>
           {/* overlay */}
           <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-primary-700/90 to-primary-700/40 z-0' aria-hidden />
           {/* content */}
           <div className='defaultPadding h-full w-full flex flex-col items-center justify-center relative'>
-            <h1 className='uppercase mb-7'>
+            <h1 className='uppercase mb-12'>
               Jaringan Dokumentasi dan Informasi Hukum<br />
               Kementrian Luar Negeri
             </h1>
-            <p className='text-neutral-200'>Temukan Informasi dan Dokumentasi Hukum Terkini dari Kementrian Luar Negeri</p>
-            <input type='text' className='mb-7' />
-            <p className='font-medium mb-7 text-neutral-100'>Pencarian Populer Dokumen Hukum JDIH</p>
+            <p className='text-neutral-200 mb-2'>Temukan Informasi dan Dokumentasi Hukum Terkini dari Kementrian Luar Negeri</p>
+            <form className='relative flex w-full mb-7'>
+              <span class="sr-only">Search</span>
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <Image src='/icons/icon-search.svg' width={20} height={20} alt='icon search' />
+              </span>
+              <input 
+                type='text'
+                placeholder='Ketik kata kunci pencarian'
+                className='placeholder:italic placeholder:text-neutral-500 h-full flex-grow rounded-l-md pl-10 py-2 pr-3 text-neutral-900' 
+              />
+              <button className='flex-grow-0 whitespace-nowrap py-2 px-3 rounded-r-md bg-secondary-300 text-neutral-900 enabled:hover:bg-secondary-500 duration-100'>Cari Dokumen</button>
+            </form>
+            <p className='font-medium mb-2 text-neutral-100'>Pencarian Populer Dokumen Hukum JDIH</p>
+            <div className='flex items-center justify-center gap-4'>
+              { HasilPencarian.map((item, index) => (
+                <Link href='' key={index}>
+                  <a key={index} className='flex gap-4 items-center bg-white hover:bg-neutral-200 duration-100 text-neutral-700 px-4 py-2 rounded-md'>
+                    <b>{item}</b>
+                    <Image src='/icons/icon-open-in-new-tab.svg' width={16} height={16} alt='icon open' />
+                  </a>
+                </Link>
+              )) }
+              <Link href='/dokumen'>
+                <a className='underline hover:text-secondary-300 duration-100'>
+                  Pencarian Lanjut
+                </a>
+              </Link>
+            </div>
+
+            {/* DOKUMEN CAROUSEL */}
             <div className='absolute bottom-0 flex bg-secondary-400 w-full py-2 text-neutral-700'>
               <button className='whitespace-nowrap buttonWhite mx-2'>Dokumen Terbaru</button>
               <div className='overflow-hidden'>
